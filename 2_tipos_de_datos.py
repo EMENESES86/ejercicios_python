d3b575e53bc40349cbc501789c6501f087ee3b02
EDISON MENESES <emeneses.developers@gmail.com>
2023-01-10 21:46:24 -0500

    Tipos de datos

---
+++ 2_tipos_de_datos.py
@@ -0,0 +1,17 @@
+a=5
+x=100
+print('el valor de x es:',x,"El valor de a es:",a)
+print(type(a))
+print("---------------")
+print(a,type(a))
+print("---------------")
+
+b='EMENESES'
+print(b,type(b))
+print("---------------")
+c=2.5065465465465
+print(c,type(c))
+print("{:.2f}".format(c))
+print("---------------")
+d=True
+print(d,type(d))
