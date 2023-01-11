c51e025c5e56033fa5e9e5c88cc092e782cea706
EDISON MENESES <emeneses.developers@gmail.com>
2023-01-10 21:44:24 -0500

    Concatenar

---
+++ 1_concatenar.py
@@ -0,0 +1,14 @@
+text1 = "Mi numero"
+text2 = 'favorito es'
+num1 = 20.5
+
+print(text1+" "+text2+" "+str(num1))
+
+print("--------------")
+print("{}{}{}".format(text1,text2,num1))
+
+print("--------------")
+print(text1,text2,num1)
+
+print("--------------")
+print(f"{text1} {text2} {num1}")
