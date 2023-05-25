a55797bf0ce63cfb3f1b32f656faf83e37ecaa26
EDISON MENESES <emeneses.developers@gmail.com>
2023-05-24 21:18:13 -0500

    CICLO FOR FIGURAS

---
+++ 009_1_ciclo_for_figuras.py
@@ -0,0 +1,80 @@
+#Ing. Edison Meneses MSc.
+#Ingeniero en Sistemas de Información
+#Magister en Sistemas de Información Mención en Inteligencia de Negocios y Analítica Datos Masivos
+#tutorias@emenesesdevelopers.com
+
+####################LISTA ACENDENTE#####################
+num  = int(input("ingrese un numero: "))
+for i in range(0,num+1,1):
+    print(i)
+
+# ingrese un numero: 5
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+        
+####################LISTA DECENDENTE#####################
+# num  = int(input("ingrese un numero: "))
+# for i in range(num,0-1,-1):
+#     print(i)
+
+# ingrese un numero: 5
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+
+####################TRIANGULO#####################
+# num  = int(input("ingrese un numero: "))
+# for i in range(1,num+1):
+#     print(i*"* ")
+    
+# ingrese un numero: 5
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+####################TRIANGULO#####################
+# num  = int(input("ingrese un numero: "))
+# for i in range(1,num+1):
+#     print(i*"* ")
+# for i in range(num-1,0,-1):
+#     print(i*"* ")
+    
+# ingrese un numero: 5
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+####################ROMBO#####################
+# num  = int(input("ingrese un numero: "))
+# for i in range(1,num+1):
+#     espacio1=(num-i)*" "
+#     print(espacio1,i*"* ")
+# for i in range(num-1,0,-1):
+#     espacio2=(num-i)*" "
+#     print(espacio2,i*"* ")
+    
+# ingrese un numero: 5
+#      * 
+#     * * 
+#    * * * 
+#   * * * *
+#  * * * * *
+#   * * * *
+#    * * *
+#     * *
+#      *
